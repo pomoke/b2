7c53449ebd436efd2a73e28c75c6bc7ba64d97cd
pomoke <pomoke@typeof.pw>
2024-01-21 21:11:28 +0800

    Extend KVM memory to load Linux image.

--- debug.sh
+++ debug.sh
@@ -2,5 +2,4 @@
 cargo b --target=x86_64-unknown-uefi
 cp target/x86_64-unknown-uefi/debug/b2.efi esp/efi/boot/bootx64.efi
 
-kvm -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp
-
+kvm -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp -m 1024M
